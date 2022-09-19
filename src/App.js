e0c017778f654b4d353a671fd0bc0b7a6315823d
Mccullahz <zylardmccullah@gmail.com>
2022-09-19 17:54:04 -0400

    Created basic UI, added calendar, imported useState, imported react-calendar, used onChange

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import React, { useState } from 'react'
+import Calendar from 'react-calendar'
 
 function App() {
+  const [value, onChange] = useState(new Date())
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-        This is my first React-App
-        </p>
-      </header>
+       <p>Calendar</p>
+      
+      <Calendar onChange={onChange} value={value}/>
+      </header> 
     </div>
   );
 }
