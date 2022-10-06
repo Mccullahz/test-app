661031077dd094c3b310fc3c028944bee932e6f1
Zylar McCullah <80295174+Mccullahz@users.noreply.github.com>
2022-10-06 10:35:59 -0400

    Added new function

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import React, { useState } from 'react'
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
 
-function App() {
+
+function Popout() {
+  <div className='Popout'>
+    <h2 className='text-center'>Select A Time To Schedule </h2>
+    <div className='Poput-container'>
+      
+    </div>
+  </div>
+
+
+}
+
+
+
+function Cal() {
   const [date, setDate] = useState(new Date());
 
   return (
-    <div className='App'>
+    <div className='Cal'>
       <h1 className='text-center'>Scheduler</h1>
       <div className='calendar-container'>
         <Calendar onChange={setDate} value={date} />
@@ -20,4 +34,4 @@ function App() {
   );
 }
 
-export default App;
+export default Cal;
