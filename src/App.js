87e14d7ee578dc411f49f150777f978871b1f3fd
Zylar McCullah <80295174+Mccullahz@users.noreply.github.com>
2022-10-07 10:45:10 -0400

    Attempting to figure out css for popout function, to no avail. Function does not give me any real output and I am unsure of why this would be.

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import ClickAwayListener from 'react-click-away-listener';
 const Popout = () => {
   const [popout, setPopout] = useState(false)
   return (
-      popout && (
+      popout (
           <ClickAwayListener onClickAway={() => setPopout(false)}>
                   <div className={'popout'}>
-                      <li>Items of the Popup</li>
+                      <li>Please print something on the screen lol</li>
                       <li>Items of the Popup</li>
                       <li>Items of the Popup</li>
                   </div>
@@ -33,7 +33,7 @@ function Cal() {
         <Calendar onChange={Popout} value={date} />
       </div>
       <p className='text-center'>
-        <span className='bold'>Selected Date:</span>{' '}
+        <span className='bold'>Today's Date:</span>{' '}
         {date.toDateString()}
       </p>
     </div>
