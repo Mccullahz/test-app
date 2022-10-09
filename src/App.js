614150de81e56f6b7989f9bde696e4bafb8e518e
Zylar McCullah <80295174+Mccullahz@users.noreply.github.com>
2022-10-09 05:35:28 -0400

    Testing

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
 import ClickAwayListener from 'react-click-away-listener';
 
-
-
 const Popout = () => {
   const [popout, setPopout] = useState(false)
   return (
@@ -14,14 +12,12 @@ const Popout = () => {
                   <div className={'popout'}>
                       <li>Please print something on the screen lol</li>
                       <li>Items of the Popup</li>
-                      <li>Items of the Popup</li>
+                      <p> Please webapp </p>
                   </div>
           </ClickAwayListener>
       )
   );
 }
-
-
 
 function Cal() {
   const [date, setDate] = useState(new Date());
